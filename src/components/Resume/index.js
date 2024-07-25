3ce70d9a9d4a158f6eeab1ae282421fb03643460
linglp <linglp@umich.edu>
2024-07-24 21:58:09 -0400

    change a way to grab pdf.worker.min

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,11 +3,7 @@ import { useState, useEffect } from 'react';
 import resumePdf from "../../assets/Resume.pdf";
 import { Document, Page, pdfjs } from "react-pdf";
 
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 
 const Resume = () => {
