be8c35e8d2a7af3785ab85139685887dc5e238e7
Lingling Peng <linglingpeng@Linglings-MacBook-Pro.local>
2025-12-11 20:47:05 -0500

    add background

--- src/components/Contact/ContactSVG.js
+++ src/components/Contact/ContactSVG.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const ContactSVG = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 790 563" fill="none">
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,7 @@ import './Hero.css';
 
 const Hero = () => {
   return (
-    <section className="hero">
+    <section className="hero" style={{ '--bg-image': `url(${process.env.PUBLIC_URL}/images/backdrop.webp)` }}>
       <h1>Welcome to My Portfolio</h1>
       <h2>Lingling Peng</h2>
       <a href="#projects" className="cta">View My Projects</a>
